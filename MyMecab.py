21aa36fbdfb72c78509200565c0fad4df74cc730
Taguchi Hiroaki <ragiko.complexe@gmail.com>
2014-10-02 16:44:52 +0900

    活用形と原型を選択出来るように修正

--- MyMecab.py
+++ MyMecab.py
@@ -2,7 +2,6 @@
 
 import MeCab
 
-
 class NodeWrapper:
     """
     nodeのデータを構造化
@@ -38,6 +37,12 @@ class NodeWrapper:
 
 class MecabParser:
     def __init__(self, doc):
+        """
+        _tagger: mecabのtagger
+        _doc: 解析する文書
+        _node_wrappers_cache: 形態素解析結果を一時的に保存
+        """
+
         self._tagger = MeCab.Tagger('')
         self._doc = doc # unicode
         self._node_wrappers_cache = []
@@ -64,6 +69,9 @@ class MecabParser:
                     )
         
     def save_node_wrappers(self):
+        """
+        nodeの解析結果を一時保存するcacheに入れる
+        """
         # http://shogo82148.github.io/blog/2012/12/15/mecab-python/
         # MeCabに渡す文字列はencode，戻ってきた文字列はdecodeする
         # MeCabに渡した文字列は必ず変数に入れておく
@@ -82,10 +90,12 @@ class MecabParser:
 
         self._node_wrappers_cache = node_wrappers
     
+    def find_words_by_pos(self, pos_set, is_base_word = False):
+        """
+        pos(品詞)から品詞にあった単語を取得
 
-    
-    def find_words_by_pos(self, pos_set):
-
+        pos_set: 品詞の集合
+        """
         if (len(self._node_wrappers_cache) == 0):
             # メソッドは、 self 引数のメソッド属性を使って、他のメソッドを呼び出すことができます
             # 参考: http://docs.python.jp/2/tutorial/classes.html
@@ -93,24 +103,61 @@ class MecabParser:
 
         node_wrappers = self._node_wrappers_cache
 
+
         if (len(pos_set) == 0):
             # pos_setが0の時は全ての単語を返却
-            return [nw.surface for nw in node_wrappers]
+            base_and_surface_pair = [(nw.base, nw.surface) for nw in node_wrappers]
+            return [self.choose_base_or_surface_word(pair, is_base_word) for pair in base_and_surface_pair]
         else:
-            return [nw.surface for nw in node_wrappers if nw.pos in pos_set]
-        
+            base_and_surface_pair = [(nw.base, nw.surface) for nw in node_wrappers if nw.pos in pos_set]
+            return [self.choose_base_or_surface_word(pair, is_base_word) for pair in base_and_surface_pair]
+
+    def choose_base_or_surface_word(self, base_and_surface_pair, is_base_word):
+        """
+        品詞の原型か活用形かを選ぶ関数
+        """
+        base = base_and_surface_pair[0] 
+        surface = base_and_surface_pair[1] 
+
+        if (is_base_word):
+            # mecabは原型の時に*を返却する時があるため、その場合は活用形を使う 
+            return surface if base == u'*' else base # return base
+        else:
+            return surface # return surface
+            
 
 if __name__ == "__main__":
     doc = u'MeCabで遊んでみよう！'
     mp = MecabParser(doc)
+
     mp.parse_dump()
+    # MeCab, 名詞, 一般, *, *, *, *, *
+    # で, 助詞, 格助詞, 一般, *, *, *, で
+    # 遊ん, 動詞, 自立, *, *, 五段・バ行, 連用タ接続, 遊ぶ
+    # で, 助詞, 接続助詞, *, *, *, *, で
+    # みよ, 動詞, 非自立, *, *, 一段, 未然ウ接続, みる
+    # う, 助動詞, *, *, *, 不変化型, 基本形, う
+    # ！, 記号, 一般, *, *, *, *, ！
 
-    pos_set = set([u"名詞", u"動詞", u"助詞"])
-    node_wrappers = mp.find_words_by_pos(pos_set)
-
+    pos_set = set([u"名詞"])
+    node_wrappers = mp.find_words_by_pos(pos_set, False)
     for n in node_wrappers:
         print n.encode("utf-8")
+    # MeCab
 
-
+    pos_set = set([u"名詞", u"動詞"])
+    node_wrappers = mp.find_words_by_pos(pos_set, False)
+    for n in node_wrappers:
+        print n.encode("utf-8")
+    # MeCab
+    # 遊ん
+    # みよ
     
+    pos_set = set([u"名詞", u"動詞"])
+    node_wrappers = mp.find_words_by_pos(pos_set, True)
+    for n in node_wrappers:
+        print n.encode("utf-8")
+    # MeCab
+    # 遊ぶ
+    # みる
 
